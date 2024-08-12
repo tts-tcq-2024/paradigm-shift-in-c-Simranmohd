8755270ca7d9cc24ca14aa4b27f57714c7c991dc
Simranmohd <120707251+Simranmohd@users.noreply.github.com>
2024-08-12 22:59:59 +0530

    Update Battery.h

--- Battery.h
+++ Battery.h
@@ -1,1 +1,11 @@
+#ifndef BATTERY_H
+#define BATTERY_H
+
+// Function declarations
+int Batterytemprange(float temp);
+int Batterysoc(float soc);
+int Batterychargerate(float chargeRate);
+int batteryIsOk(float temp, float soc, float chargeRate);
+
+#endif // BATTERY_H
 
